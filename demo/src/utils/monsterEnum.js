580770b70458bd854ad696bbbf71004e90c5a859
kevin <kevinrev26@gmail.com>
2018-10-03 15:28:44 -0600

    Refactoring demo

---
+++ demo/src/utils/monsterEnum.js
@@ -0,0 +1,107 @@
+import Baal from '../assets/models/baal.gltf'
+import BadChicken from  '../assets/models/bad-chicken.gltf'
+import Bat from  '../assets/models/bat.gltf'
+import Bear from '../assets/models/bear.gltf'
+import Beetle from '../assets/models/beetle.gltf'
+import Butterfly from '../assets/models/butterfly.gltf'
+import Cactus from '../assets/models/cactus.gltf'
+import Cerberus from '../assets/models/cerberus.gltf'
+import Devil from '../assets/models/devil.gltf'
+import Duck from '../assets/models/duck.gltf'
+import Dwarf from '../assets/models/dwarf.gltf'
+import Egg from '../assets/models/egg.gltf'
+import Frog from '../assets/models/frog.gltf'
+import Ghost from '../assets/models/ghost.gltf'
+import MetalGuitar from '../assets/models/metal-guitar.gltf'
+import Minion from '../assets/models/minion.gltf'
+import Ness from '../assets/models/ness.gltf'
+import Ogre from '../assets/models/ogre.gltf'
+import Penguin from '../assets/models/penguin.gltf'
+import RockWorm from '../assets/models/rock-worm.gltf'
+import Rocky from '../assets/models/rocky.gltf'
+import Scorpion from '../assets/models/scorpion.gltf'
+import Serpent from '../assets/models/serpent.gltf'
+import Spider from '../assets/models/spider.gltf'
+import TheThing from '../assets/models/the-thing.gltf'
+import Toad from '../assets/models/toad.gltf'
+import Tree from '../assets/models/tree.gltf'
+import Troll from '../assets/models/troll.gltf'
+import Tucan from '../assets/models/Tucan.gltf'
+import Vampire from '../assets/models/vampire.gltf'
+import Wolf from '../assets/models/wolf.gltf'
+import Worm from '../assets/models/worm.gltf'
+
+export const monster3D = monster => {
+    switch (monster) {
+        case "Baal":
+            return Baal;
+        case "BadChicken":
+            return BadChicken;
+        case "Bat":
+            return Bat;
+        case "Bear":
+            return Bear;
+        case "Beetle":
+            return Beetle;
+        case "Butterfly":
+            return Butterfly;
+        case "Cactus":
+            return Cactus;
+        case "Cerberus":
+            return Cerberus;
+        case "Devil":
+            return Devil;
+        case "Duck":
+            return Duck;
+        case "Dwarf":
+            return Dwarf;
+        case "Egg":
+            return Egg;
+        case "Frog":
+            return Frog;
+        case "Ghost":
+            return Ghost;
+        case "MetalGuitar":
+            return MetalGuitar;
+        case "Minion":
+            return Minion;
+        case "Ness":
+            return Ness;
+        case "Ogre":
+            return Ogre;
+        case "Penguin":
+            return Penguin;
+        case "RockWorm":
+            return RockWorm;
+        case "Rocky":
+            return Rocky;
+        case "Scorpion":
+            return Scorpion;
+        case "Serpent":
+            return Serpent;
+        case "Spider":
+            return Spider;
+        case "TheThing":
+            return TheThing;
+        case "Toad":
+            return Toad;
+        case "Tree":
+            return Tree;
+        case "Troll":
+            return Troll;
+        case "Tucan":
+            return Tucan;
+        case "Vampire":
+            return Vampire;
+        case "Wolf":
+            return Wolf;
+        case "Worm":
+            return Worm;
+    }
+}
+
+
+export const monsters = ["Baal","BadChicken", "Bat", "Bear", "Beetle", "Butterfly", "Butterfly", "Cactus", "Cerberus", "Devil", "Duck", "Dwarf", "Egg", "Frog",
+                        "Ghost", "MetalGuitar", "Minion", "Ness", "Ogre", "Penguin", "RockWorm", "Rocky", "Scorpion", "Serpent", "Spider", "TheThing", "Toad", 
+                        "Tree", "Troll", "Tucan", "Vampire", "Wolf", "Worm"];
+
