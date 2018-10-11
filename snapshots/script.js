524e668a0464023de4807bffda3486f4996963f9
racsoraul <racsoraul@gmail.com>
2018-10-10 19:24:19 -0600

    Replaced raw string concatenation by string templates. Improved identation. Removed unnecessary comments.

--- snapshots/script.js
+++ snapshots/script.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer');
 const dataUri = require('image-data-uri');
-const url = 'http://localhost:';
+const url = 'http://localhost';
 const filePath = './snapshots/images';
-const monsters = ["Baal","BadChicken", "Bat", "Bear", "Beetle", "Butterfly", "Cactus", "Cerberus", "Devil", "Duck", "Dwarf", "Egg", "Frog",
-"Ghost", "MetalGuitar", "Minion", "Ness", "Ogre", "Penguin", "RockWorm", "Rocky", "Scorpion", "Serpent", "Spider", "TheThing", "Toad", 
-"Tree", "Troll", "Tucan", "Vampire", "Wolf", "Worm"];
+const monsters = ["Baal", "BadChicken", "Bat", "Bear", "Beetle", "Butterfly", "Cactus", "Cerberus", "Devil", "Duck", "Dwarf", "Egg", "Frog",
+    "Ghost", "MetalGuitar", "Minion", "Ness", "Ogre", "Penguin", "RockWorm", "Rocky", "Scorpion", "Serpent", "Spider", "TheThing", "Toad",
+    "Tree", "Troll", "Tucan", "Vampire", "Wolf", "Worm"];
 
 
 const takeSnaps = async (port) => {
@@ -12,15 +12,15 @@ const takeSnaps = async (port) => {
     const page = await browser.newPage();
 
     console.log("Entering page...");
-    await page.goto(url+port);
+    await page.goto(`${url}:${port}`);
 
     console.log("Generating snaps, may take a while");
 
     for (index in monsters) {
-        const imageEncoded = await page.evaluate( async (monster) => {
+        const imageEncoded = await page.evaluate(async (monster) => {
 
             renderIt(monster, document.querySelector("#demo"));
-            await((ms = 2000) =>
+            await ((ms = 2000) =>
                 new Promise((resolve, reject) => {
                     try {
                         setTimeout(resolve, ms)
@@ -28,22 +28,18 @@ const takeSnaps = async (port) => {
                         reject(error)
                     }
                 }))()
-            //debugger
+
             return document.querySelector("canvas").toDataURL('image/png')
-            
-            
+
         }, monsters[index])
-        console.log("Generating: " + monsters[index] + " ...")
+        console.log(`Generating: ${monsters[index]} ...`)
         dataUri.outputFile(imageEncoded, filePath + '/' + monsters[index] + '_' + Date.now())
-        .then(res => console.log(res));
-    } 
+            .then(res => console.log(res));
+    }
     console.log("Closing connection...");
     await browser.close();
-    console.log("DONE!");   
+    console.log("DONE!");
 
 }
 
-
-//levantar express
-//runPuppeter();
 module.exports = takeSnaps;
