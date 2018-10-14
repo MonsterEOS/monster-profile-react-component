157339aa077a4c24afb80e0824d12f30ec762d73
Kevin Rivera <kevinrev26@gmail.com>
2018-10-14 01:41:36 -0600

    Files for camara configuration, models and 3Dmatrix added to the component

--- demo/src/utils/monsterEnum.js
+++ demo/src/utils/monsterEnum.js
@@ -105,3 +105,5 @@ export const monsters = ["Baal","BadChicken", "Bat", "Bear", "Beetle", "Butterfl
                         "Ghost", "MetalGuitar", "Minion", "Ness", "Ogre", "Penguin", "RockWorm", "Rocky", "Scorpion", "Serpent", "Spider", "TheThing", "Toad", 
                         "Tree", "Troll", "Tucan", "Vampire", "Wolf", "Worm"];
 
+export const decorations = ["neutral", "animal", "fire", "ice", "metal", "poison", "water", "earth", "wood", "light", "dead", "undead"]
+
